d726e3faef31da064d05727087f3189d1ebb147f
zhang-guodong <zhangguodong1996@qq.com>
2018-01-16 23:12:41 +0800

    创建levenshtein算法的头文件

---
+++ levenshtein.h
@@ -0,0 +1,9 @@
+#ifndef _LEVENSHTEIN_H
+#define _LEVENSHTEIN_H
+#include <vector>
+class levenshtein
+{
+    public:
+    int levenshtein(vector<int> str1, vector<int> str2);
+};
+#endif
---
+++ levenshteinRecursive.h
@@ -0,0 +1,2 @@
+#include <vector>
+extern int levenshtein(vector<int> str1, vector<int> str2);
